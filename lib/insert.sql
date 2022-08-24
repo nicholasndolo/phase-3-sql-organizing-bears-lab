1650b24816a9da4d05fe8c40e74da592e00efefa
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-24 16:30:48 +0300

    commit tree 9a5f131a11a9386e0c786e2d188972b2f5de0f42

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chololate', 4, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'black', 'male', 'aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 1, 'female', 'white', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'male', 'white', 'calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'male', 'black', 'aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'male', 'white', 'calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'female', 'black', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 3, 'male', 'grey', 'cool', 1);
+
+
+
+
+
+
+
+
